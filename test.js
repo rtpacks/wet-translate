a7fc2a51d3e9e5bcf752260682959a02233eeaa4
azin-cn <luckywords@foxmail.com>
2022-07-11 17:56:51 +0800

    test

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
+const http = require('http')
 const URL = require("url").URL;
 const qs = require("querystring");
 const config = require("./wet.config.json");
+
 
 console.log(config.servicer);
 console.log(qs.stringify({ q: ["lisi", "zs"] }));
@@ -10,13 +12,14 @@ url.search =
   "q=apple&from=auto&to=auto&appid=20210921000952000&salt=azin-cn&sign=3a8736c54facc924ce09292957b78757";
 
 // console.log(url)
+const client = http.get(url, res => {
+  
+})
 
-const l1 =
-  "http://api.fanyi.baidu.com/api/trans/vip/translate?q=apple&salt=azin-cn&appid=20210921000952000&sign=3a8736c54facc924ce09292957b78757";
-const l2 =
-  "http://api.fanyi.baidu.com/api/trans/vip/translate?q=apple&salt=azin-cn&appid=20210921000952000&sign=3a8736c54facc924ce09292957b78757";
+client.on('error', err => {
+  console.log('错误')
+})
 
-console.log(l1 == l2);
 
 // console.log(encodeURI(`${1+"\n"}`))
 console.log(
@@ -30,4 +33,4 @@ const s = "Hello";
 console.log(`%c${s}`, "color: red;");
 console.log("\033[41;30m DONE \033[40;32m .json build success\033[0m");
 
-console.log("\x1B[43m 原文 " + "\x1B[40;33m " + l1 + "\033[0m");
+// console.log("\x1B[43m 原文 " + "\x1B[40;33m " + l1 + "\033[0m");
